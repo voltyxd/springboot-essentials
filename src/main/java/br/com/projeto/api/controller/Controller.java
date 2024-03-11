89c46e1e17e47d28f26e00531c6b09ad1ada6316
Andre Gustavo <andgouveiap@gmail.com>
2024-03-11 16:05:41 -0300

    listando dados com o comando findAll()

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,5 +1,7 @@
 package br.com.projeto.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,11 @@ public class Controller {
     @PostMapping("/api")
     public Pessoa cadastrar(@RequestBody Pessoa pessoa) {
         return repositorio.save(pessoa);
+    }
+
+    @GetMapping("/api")
+    public List<Pessoa> buscar() {
+        return repositorio.findAll();
     }
     
     @GetMapping("")
--- src/main/java/br/com/projeto/api/repository/Repositorio.java
+++ src/main/java/br/com/projeto/api/repository/Repositorio.java
@@ -1,5 +1,7 @@
 package br.com.projeto.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import br.com.projeto.api.model.Pessoa;
 @Repository
 public interface Repositorio extends CrudRepository<Pessoa, Integer> {
     
+    List<Pessoa> findAll();
 }
