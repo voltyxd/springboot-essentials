807114f19c04c257e5a1ba4a8e339ebff30c8243
Andre Gustavo <andgouveiap@gmail.com>
2024-03-11 16:42:58 -0300

    alterando dados com save()

--- src/main/java/br/com/projeto/api/controller/Controller.java
+++ src/main/java/br/com/projeto/api/controller/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +32,11 @@ public class Controller {
     @GetMapping("/api/{id}")
     public Pessoa buscarPorId(@PathVariable int id) {
         return repositorio.findById(id);
+    }
+
+    @PutMapping("/api")
+    public Pessoa editar(@RequestBody Pessoa pessoa) {
+        return repositorio.save(pessoa);
     }
     
     @GetMapping("")
