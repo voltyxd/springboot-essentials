11c64903463a2a33886548299f0321430d4bedf7
Andre Gustavo <andgouveiap@gmail.com>
2024-03-11 15:12:41 -0300

    adicionando modelos

---
+++ src/main/java/br/com/projeto/api/model/Pessoa.java
@@ -0,0 +1,26 @@
+package br.com.projeto.api.model;
+
+public class Pessoa {
+    
+    // Atributo
+    private String nome;
+    private int idade;
+
+    // Get e Set
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
