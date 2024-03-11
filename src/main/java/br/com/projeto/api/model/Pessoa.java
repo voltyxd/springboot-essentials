cf065818f1c3f557499b105e6cdf7fdb68d619de
Andre Gustavo <andgouveiap@gmail.com>
2024-03-11 15:37:54 -0300

    configurando modelo para gerar tabela

--- src/main/java/br/com/projeto/api/model/Pessoa.java
+++ src/main/java/br/com/projeto/api/model/Pessoa.java
@@ -1,8 +1,19 @@
 package br.com.projeto.api.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
     
     // Atributo
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String nome;
     private int idade;
 
@@ -21,6 +32,14 @@ public class Pessoa {
 
     public void setIdade(int idade) {
         this.idade = idade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
 }
