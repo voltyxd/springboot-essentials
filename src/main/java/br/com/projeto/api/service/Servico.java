8de0edd5fbf86adba432a5eac7c3022f0c362f2b
Andre Gustavo <andgouveiap@gmail.com>
2024-03-12 10:57:16 -0300

    annotation @Component

--- src/main/java/br/com/projeto/api/service/Servico.java
+++ src/main/java/br/com/projeto/api/service/Servico.java
@@ -1,8 +1,16 @@
 package br.com.projeto.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.projeto.api.model.Mensagem;
 
 @Service
 public class Servico {
+
+    @Autowired
+    private Mensagem mensagem;
+
+    
     
 }
